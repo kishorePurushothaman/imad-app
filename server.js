d54dbcbf35bd472c018008e1392f894a4f7dd333
kishorePurushothaman <kishorenaidu1596@gmail.com>
2017-11-29 11:40:49 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ app.get('/', function (req, res) {
 });
 
 app.get('/artical-one',function(req,res){
-    res.send('artical one is requested and it will be resived later');
+     res.sendFile(path.join(__dirname, 'ui', 'artical-one.html'));
 });
 app.get('/artical-two',function(req,res){
     res.send('artical two is requested and it will be resived later');
