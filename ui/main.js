94f1a1a7d8f8b487970f54624f3b886a7a5afc52
kishorePurushothaman <kishorenaidu1596@gmail.com>
2017-11-30 17:40:33 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -2,3 +2,9 @@ console.log('Loaded!');
 
 var element=document.getElementById('main-text');
 element.innerHTML='new value';
+
+
+var img=document.getElementById('kish');
+img.onclick = function() {
+    img.style.marginleft='100px';
+};
